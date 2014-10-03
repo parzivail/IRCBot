22c2965168eaeb62839f7c0e0dee1aa3f30e504a
Colby Newman <colby.newman2000@gmail.com>
2014-10-02 20:07:23 -0400

    Fixed "hi" not working

--- IRCBot/Program.cs
+++ IRCBot/Program.cs
@@ -112,7 +112,7 @@ namespace IRCBot
                 }
 
 
-                if ((datal.Contains("hey") || datal.Contains("hello")) && datal.Contains("parzibot"))
+                if ((datal.Contains("hey") || datal.Contains("hello") || datal.Contains("hi")) && datal.Contains("parzibot"))
                 {
                     string sender = data.Split('!')[0].Replace(":", "");
                     Console.WriteLine(data+"\n");
@@ -125,12 +125,17 @@ namespace IRCBot
                     chat(config.channel, "Copycat now " + copycat.ToString());
                 }
 
+                if (copycat)
+                {
+                    chat(config.channel, data.Split(new char[] { ':' }, 3)[2]);
+                }
+
                 if (datal.Contains("when parzibot"))
                 {
                     me(config.channel, datal.Split(new char[] { ':' }, 3)[2].Replace("when parzibot ", ""));
                 }
 
-                if (datal.Contains("parzibot") && datal.Contains("leave"))
+                if (datal.Contains("parzibot") && datal.Contains("leave") && (datal.Contains("irc") || datal.Contains("channel") || datal.Contains("chat")))
                 {
                     sendData("QUIT", null);
                     shouldRun = false;
@@ -141,36 +146,6 @@ namespace IRCBot
                     string sender = data.Split('!')[0].Replace(":", "");
                     chat(config.channel, sender + ": It's over 9000!");
                 }
-
-                if (copycat)
-                {
-                    chat(config.channel, data.Split(new char[] { ':' }, 3)[2]);
-                }
-
-                /*if (ex.Length > 4) //is the command received long enough to be a bot command?
-                {
-                    string command = ex[3]; //grab the command sent
-
-                    switch (command)
-                    {
-                        case ":!join":
-                            sendData("JOIN", ex[4]);
-                            //if the command is !join send the "JOIN" command to the server with the parameters set by the user
-                            break;
-                        case ":!say":
-                            sendData("PRIVMSG", ex[2] + " " + ex[4]);
-                            //if the command is !say, send a message to the chan (ex[2]) followed by the actual message (ex[4]).
-                            break;
-                        case ":!quit":
-                            sendData("QUIT", ex[4]);
-                            //if the command is quit, send the QUIT command to the server with a quit message
-                            shouldRun = false;
-                            //turn shouldRun to false - the server will stop sending us data so trying to read it will not work and result in an error. This stops the loop from running and we will close off the connections properly
-                            break;
-                    }
-                 
-                }
-                 * */
             }
         }
 
