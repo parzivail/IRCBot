4d37bf1cd38361488426692e0d592d8a1d3b190c
Colby Newman <colby.newman2000@gmail.com>
2015-08-10 19:12:08 -0400

    Extracted functions

--- IRCBot/IRCBot.cs
+++ IRCBot/IRCBot.cs
@@ -12,7 +12,7 @@ namespace IRCBot
     internal class IRCBot : IDisposable
     {
         private TcpClient IRCConnection = null;
-        private IRCConfig config;
+        public IRCConfig config;
         private NetworkStream ns = null;
         private StreamReader sr = null;
         private StreamWriter sw = null;
@@ -88,7 +88,6 @@ namespace IRCBot
             string[] ex;
             string data;
             bool shouldRun = true;
-            bool copycat = false;
             string datal = "";
             while (shouldRun)
             {
@@ -108,71 +107,7 @@ namespace IRCBot
                         me(config.channel, "whirrs idly");
                     }
                 }
-
-
-
-                if (ex[0] == "PING")  //respond to pings
-                {
-                    sendData("PONG", ex[1]);
-                }
-                else if ((ContainsP(datal, "hey") || ContainsP(datal, "hello") || ContainsP(datal, "hi")) && ContainsP(datal, "parzibot"))
-                {
-                    string sender = data.Split('!')[0].Replace(":", "");
-                    Console.WriteLine(data + "\n");
-                    chat(config.channel, "Hello, " + sender + "!");
-                }
-                else if (ContainsP(datal, "-p copycat"))
-                {
-                    copycat = !copycat;
-                    chat(config.channel, "Copycat now " + copycat.ToString());
-                }
-                else if (ContainsP(datal, "functions") && ContainsP(datal, "-p"))
-                {
-                    chat(config.channel, "My functions:");
-                    chat(config.channel, "\\");
-                    string[] f = { "Time: just ask what time it is", "Copycat: 'parzibot copycat'", "Ping: 'parzibot, ping <server>'", "Power Level: 'parzibot, what's the power level' or similar", "Leave: 'parzibot, leave irc' or similar" };
-                    foreach (string func in f)
-                    {
-                        chat(config.channel, " |- " + func);
-                    }
-                }
-                else if (copycat)
-                {
-                    try
-                    {
-                        chat(config.channel, data.Split(new char[] { ':' }, 3)[2]);
-                    }
-                    catch
-                    {
-                        chat(config.channel, "Something just happened in chat that I can't repeat!");
-                    }
-                }
-                else if (ContainsP(datal, "-!p "))
-                {
-                    me(config.channel, datal.Split(new char[] { ':' }, 3)[2].Replace("-!p ", ""));
-                }
-                else if (ContainsP(datal, "what time is it"))
-                {
-                    chat(config.channel, "It is currently " + DateTime.Now.ToString("h:mm:ss tt") + " Eastern Time (US and Canada)");
-                }
-                else if (ContainsP(datal, "parzibot, ping "))
-                {
-                    string web = datal.Replace("parzibot, ping ", "\u1106").Split('\u1106')[1];
-                    try
-                    {
-                        PingReply p = new Ping().Send(web);
-                        chat(config.channel, "Pinged " + web + ": " + p.Status + ", " + p.RoundtripTime + "ms");
-                    }
-                    catch
-                    {
-                        chat(config.channel, "Unable to ping " + web);
-                    }
-                }
-                else if (ContainsP(datal, "parzibot") && ContainsP(datal, "power level"))
-                {
-                    string sender = data.Split('!')[0].Replace(":", "");
-                    chat(config.channel, sender + ": It's over 9000!");
-                }
+                IRCResponse.doWork(this, ex, data, datal);                
             }
         }
 
---
+++ IRCBot/IrcResponse.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.NetworkInformation;
+
+namespace IRCBot
+{
+    class IRCResponse
+    {
+        public static void doWork(IRCBot bot, string[] ex, string data, string datal)
+        {
+            if (ex[0] == "PING")  //respond to pings
+            {
+                bot.sendData("PONG", ex[1]);
+            }
+            else if ((bot.ContainsP(datal, "hey") || bot.ContainsP(datal, "hello") || bot.ContainsP(datal, "hi")) && bot.ContainsP(datal, "parzibot"))
+            {
+                string sender = data.Split('!')[0].Replace(":", "");
+                Console.WriteLine(data + "\n");
+                bot.chat(bot.config.channel, "Hello, " + sender + "!");
+            }
+            else if (bot.ContainsP(datal, "functions") && bot.ContainsP(datal, "-p"))
+            {
+                bot.chat(bot.config.channel, "My functions:");
+                bot.chat(bot.config.channel, "\\");
+                string[] f = { "Time: just ask what time it is", "Copycat: 'parzibot copycat'", "Ping: 'parzibot, ping <server>'", "Power Level: 'parzibot, what's the power level' or similar", "Leave: 'parzibot, leave irc' or similar" };
+                foreach (string func in f)
+                {
+                    bot.chat(bot.config.channel, " |- " + func);
+                }
+            }
+            else if (bot.ContainsP(datal, "-!p "))
+            {
+                bot.me(bot.config.channel, datal.Split(new char[] { ':' }, 3)[2].Replace("-!p ", ""));
+            }
+            else if (bot.ContainsP(datal, "what time is it"))
+            {
+                bot.chat(bot.config.channel, "It is currently " + DateTime.Now.ToString("h:mm:ss tt") + " Eastern Time (US and Canada)");
+            }
+            else if (bot.ContainsP(datal, "parzibot, ping "))
+            {
+                string web = datal.Replace("parzibot, ping ", "\u1106").Split('\u1106')[1];
+                try
+                {
+                    PingReply p = new Ping().Send(web);
+                    bot.chat(bot.config.channel, "Pinged " + web + ": " + p.Status + ", " + p.RoundtripTime + "ms");
+                }
+                catch
+                {
+                    bot.chat(bot.config.channel, "Unable to ping " + web);
+                }
+            }
+            else if (bot.ContainsP(datal, "parzibot") && bot.ContainsP(datal, "power level"))
+            {
+                string sender = data.Split('!')[0].Replace(":", "");
+                bot.chat(bot.config.channel, sender + ": It's over 9000!");
+            }
+        }
+    }
+}
